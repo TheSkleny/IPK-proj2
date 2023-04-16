6cdf023ce169a68290fad459025dc3389d17f965
David Sklenar <dav.sklen@gmail.com>
2023-04-16 12:36:08 +0000

    feat: pcap handle function

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -10,6 +10,51 @@ void signal_handler(int signum) {
     exit(signum);
 }
 
+pcap_t* create_pcap_handle(char* device, char* filter)
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = NULL;
+    pcap_if_t* devices = NULL;
+    struct bpf_program bpf;
+    bpf_u_int32 netmask;
+    bpf_u_int32 srcip;
+
+    // If no network interface (device) is specfied, get the first one.
+    if (!*device) {
+    	if (pcap_findalldevs(&devices, errbuf)) {
+            cerr << "pcap_findalldevs(): " << errbuf << endl;
+            return NULL;
+        }
+        strcpy(device, devices[0].name);
+    }
+
+    // Get network device source IP address and netmask.
+    if (pcap_lookupnet(device, &srcip, &netmask, errbuf) == PCAP_ERROR) {
+        cerr << "pcap_lookupnet: " << errbuf << endl;
+        return NULL;
+    }
+
+    // Open the device for live capture.
+    handle = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        cerr << "pcap_open_live(): " << errbuf << endl;
+        return NULL;
+    }
+
+    // Convert the packet filter epxression into a packet filter binary.
+    if (pcap_compile(handle, &bpf, filter, 0, netmask) == PCAP_ERROR) {
+        cerr << "pcap_compile(): " << pcap_geterr(handle) << endl;
+        return NULL;
+    }
+
+    // Bind the packet filter to the libpcap handle.
+    if (pcap_setfilter(handle, &bpf) == PCAP_ERROR) {
+        cerr << "pcap_setfilter(): " << pcap_geterr(handle) << endl;
+        return NULL;
+    }
+
+    return handle;
+}
 
 
 int main(int argc, char* argv[]) {
@@ -127,7 +172,7 @@ int main(int argc, char* argv[]) {
     filter = options.arp + options.icmp4 + options.icmp6 + options.igmp + options.mld + options.tcp + options.udp + options.ndp;
     
     
-
+    pcap_t* handle = create_pcap_handle((char *)options.interface_name.c_str(), (char *)filter.c_str());
     
 
 
