cd2f8d739f06615c245039ac222d29c56ba1f8b8
David Sklenar <dav.sklen@gmail.com>
2023-04-16 11:04:49 +0000

    feat: argument parsing done

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -2,32 +2,18 @@
 // Author: David Sklenář - xsklen14
 // Date: 2023/03/27
 
-#include <getopt.h>
-#include <signal.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <pcap.h>
-#include <arpa/inet.h>
-#include <netinet/tcp.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/ether.h>
-#include <netinet/ip6.h>
-
-using namespace std;
+#include "ipk-sniffer.h"
 
 
-// stolen from https://stackoverflow.com/questions/1052746/getopt-does-not-parse-optional-arguments-to-parameters
-#define OPTIONAL_ARGUMENT_IS_PRESENT \
-    ((optarg == NULL && optind < argc && argv[optind][0] != '-') \
-     ? (bool) (optarg = argv[optind++]) \
-     : (optarg != NULL))
-
-
+void signal_handler(int signum) {
+    // Terminate program
+    exit(signum);
+}
 
 
 
 int main(int argc, char* argv[]) {
+    string filter = "";
 
     static struct option long_options[] = {
         {"interface", optional_argument, 0, 'i'},
@@ -40,6 +26,7 @@ int main(int argc, char* argv[]) {
         {"mld", no_argument, 0, 'm'},
         {"port", required_argument, 0, 'p'},
         {"num", required_argument, 0, 'n'},
+        {"ndp", no_argument, 0, 'd'},
         {0, 0, 0, 0}
     };
 
@@ -47,48 +34,57 @@ int main(int argc, char* argv[]) {
     int opt;
 
     // Set default values
-    string interface_name = "";
-    int port = -1;
-    bool arp = false, icmp4 = false, icmp6 = false, igmp = false, mld = false, tcp = false, udp = false;
-    int num = 1;
-    while ((opt = getopt_long(argc, argv, "i::p:tua46gmn:", long_options, &option_index)) != -1) {
+    options.interface_name = "";
+    options.port = -1;
+    options.arp = "";
+    options.icmp4 = "";
+    options.icmp6 = "";
+    options.igmp = "";
+    options.mld = "";
+    options.tcp = "";
+    options.udp = "";
+    options.num = 1;
+    while ((opt = getopt_long(argc, argv, "i::p:tua46gmdn:", long_options, &option_index)) != -1) {
         //cout << "option: " << static_cast<char>(opt) << endl;
         switch (opt) {
         case 'i':
             if (OPTIONAL_ARGUMENT_IS_PRESENT) {
-                interface_name = optarg;
+                options.interface_name = optarg;
             }
             break;
         case 'p':
-            port = atoi(optarg);
+            options.port = atoi(optarg);
             break;
         case 't':
-            tcp = true;
+            options.tcp = "tcp ";
             break;
         case 'u':
-            udp = true;
+            options.udp = "udp ";
             break;
         case 'a':
-            arp = true;
+            options.arp = "arp ";
             break;
         case '4':
-            icmp4 = true;
+            options.icmp4 = "icmp4 ";
             break;
         case '6':
-            icmp6 = true;
+            options.icmp6 = "icmp6 ";
             break;
         case 'g':
-            igmp = true;
+            options.igmp = "igmp ";
             break;
         case 'm':
-            mld = true;
+            options.mld = "mld ";
+            break;
+        case 'd':
+            options.ndp = "ndp ";
             break;
         case 'n':
-            num = atoi(optarg);
+            options.num = atoi(optarg);
             break;
         case ':':
             if (optopt == 'i') {
-                interface_name = "";
+                options.interface_name = "";
             } else {
                 cerr << "Option requires an argument: " << static_cast<char>(optopt) << endl;
             }
@@ -102,11 +98,11 @@ int main(int argc, char* argv[]) {
         }
 
     }
-    if (interface_name == "" || argc == 1) {
+    if (options.interface_name == "" || argc == 1) {
         char error_buffer[PCAP_ERRBUF_SIZE];
         pcap_if_t* all_interfaces;
         if (pcap_findalldevs(&all_interfaces, error_buffer) == -1) {
-            cout << "Error: " << error_buffer << endl;
+            cerr << "Error: " << error_buffer << endl;
             return 1;
         }
         vector<string> interface_names;
@@ -115,22 +111,24 @@ int main(int argc, char* argv[]) {
         }
         pcap_freealldevs(all_interfaces);
         for (const auto& name : interface_names) {
-            cout << name << endl;
+            cerr << name << endl;
         }
         return 0;
     }
 
+    signal(SIGINT, signal_handler);
+
+    if (options.port != -1 && (options.port < 0 || options.port > MAX_PORT)) {
+        cerr << "Invalid port number: " << options.port << endl;
+        return 1;
+    }
+
+
+    filter = options.arp + options.icmp4 + options.icmp6 + options.igmp + options.mld + options.tcp + options.udp + options.ndp;
+    
     
 
-    if (pcap_descriptor = open_pcap_socket(interface, filter)) {
-        
-    }
-    // Capture packets and analyze them
-    pcap_loop(handle, num, analyze_packet, reinterpret_cast<u_char*>(&port));
-
-    // Close the packet capture handle
-    pcap_close(handle);
-
+    
 
 
     return 0;
